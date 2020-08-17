eef8de120df339798b3a77ed7ef2e069ecec9d8b
Daniel Okoro <danielochinasa@gmail.com>
2020-08-18 00:11:32 +0100

    Updates

---
+++ precache-manifest.f9a97a50877d6aa400196f2620f0fb52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a28cf60d39453319b0dd74f2b88317",
+    "url": "/React-pring/index.html"
+  },
+  {
+    "revision": "69995053079329874529",
+    "url": "/React-pring/static/css/main.2a07847b.chunk.css"
+  },
+  {
+    "revision": "080c4dcc265261a47e4a",
+    "url": "/React-pring/static/js/2.d1e2d7a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-pring/static/js/2.d1e2d7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69995053079329874529",
+    "url": "/React-pring/static/js/main.a36f3330.chunk.js"
+  },
+  {
+    "revision": "358065bc8c7e5c2e3a94",
+    "url": "/React-pring/static/js/runtime-main.e4c17fb3.js"
+  }
+]);
